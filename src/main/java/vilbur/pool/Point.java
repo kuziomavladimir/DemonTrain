a0da101d68dab17b075da78a89c174665f181e6b
kuziomavladimir <kuziomavladimir@yandex.ru>
2020-12-06 15:29:21 +0300

    vovan

---
+++ src/main/java/vilbur/pool/Point.java
@@ -0,0 +1,13 @@
+package vilbur.pool;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+}
