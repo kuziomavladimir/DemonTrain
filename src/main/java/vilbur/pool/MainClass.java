0b16bf07b8415773d79f4dc1bbed44b39a569746
kuziomavladimir <kuziomavladimir@yandex.ru>
2020-12-06 14:35:35 +0300

    add new project

---
+++ src/main/java/vilbur/pool/MainClass.java
@@ -0,0 +1,18 @@
+package vilbur.pool;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        double x, y;
+        System.out.println("Введите количество НЕстёртых точек");
+        n = scanner.nextInt();
+
+
+
+
+    }
+}
